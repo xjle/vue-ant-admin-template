9440410e06ede76b5273deb5968bed3deffafb81
xjiale <1637898906@qq.com>
2021-07-02 14:11:10 +0800

    自定义配置

--- src/api/login.js
+++ src/api/login.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
+const baseUrl = process.env.NODE_ENV === 'development'?'portal-admin-api':window.BASE_URL
 Axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 
-export const login = obj=>Axios({method:'post',url:'/portal-admin-api/login/login.do',data:obj});
+export const login = obj=>Axios({method:'post',url:`/${baseUrl}/login/login.do`,data:obj});
 // export const resetPassword = obj=>Axios.post('/portal-admin-api/login/forgetPassword.do',obj);
-export const logout = ()=>Axios.get('/portal-admin-api/logout.do')
+export const logout = ()=>Axios.get(`/${baseUrl}/logout.do`)
